b6f4cf151dfaa70b0edac74f0c539ba70e586ffb
Ananda-lin <58536803+Ananda-lin@users.noreply.github.com>
2021-08-20 17:01:18 +0800

    Add files via upload

---
+++ 0805/0805/Form1.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace _0805
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.btn_OpenFile = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btn_Start = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.lab_totaltime = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.lab_time = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lab_end = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(74, 83);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(67, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // textBox1
+            // 
+            this.textBox1.AcceptsReturn = true;
+            this.textBox1.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.textBox1.Location = new System.Drawing.Point(70, 36);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "0";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(62, 168);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(325, 216);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(204, 36);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.Text = "0";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(155, 88);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 6;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(12, 400);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(412, 234);
+            this.dataGridView2.TabIndex = 13;
+            // 
+            // btn_OpenFile
+            // 
+            this.btn_OpenFile.Location = new System.Drawing.Point(430, 16);
+            this.btn_OpenFile.Name = "btn_OpenFile";
+            this.btn_OpenFile.Size = new System.Drawing.Size(149, 35);
+            this.btn_OpenFile.TabIndex = 9;
+            this.btn_OpenFile.Text = "打开文件";
+            this.btn_OpenFile.UseVisualStyleBackColor = true;
+            this.btn_OpenFile.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.SystemColors.Info;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(645, 15);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(149, 84);
+            this.listBox1.TabIndex = 10;
+            this.listBox1.SelectedValueChanged += new System.EventHandler(this.listBox1_SelectedValueChanged_1);
+            // 
+            // btn_Start
+            // 
+            this.btn_Start.Location = new System.Drawing.Point(430, 64);
+            this.btn_Start.Name = "btn_Start";
+            this.btn_Start.Size = new System.Drawing.Size(149, 35);
+            this.btn_Start.TabIndex = 12;
+            this.btn_Start.Text = "开始";
+            this.btn_Start.UseVisualStyleBackColor = true;
+            this.btn_Start.Click += new System.EventHandler(this.btn_Start_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(813, 16);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(211, 84);
+            this.listBox2.TabIndex = 11;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(430, 117);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(931, 755);
+            this.dataGridView1.TabIndex = 14;
+            // 
+            // lab_totaltime
+            // 
+            this.lab_totaltime.AutoSize = true;
+            this.lab_totaltime.Location = new System.Drawing.Point(1057, 33);
+            this.lab_totaltime.Name = "lab_totaltime";
+            this.lab_totaltime.Size = new System.Drawing.Size(88, 17);
+            this.lab_totaltime.TabIndex = 15;
+            this.lab_totaltime.Text = "lab_totaltime";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(291, 26);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "timer开始";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(291, 56);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 17;
+            this.button3.Text = "结束";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // lab_time
+            // 
+            this.lab_time.AutoSize = true;
+            this.lab_time.Location = new System.Drawing.Point(1060, 82);
+            this.lab_time.Name = "lab_time";
+            this.lab_time.Size = new System.Drawing.Size(61, 17);
+            this.lab_time.TabIndex = 18;
+            this.lab_time.Text = "lab_time";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lab_end
+            // 
+            this.lab_end.AutoSize = true;
+            this.lab_end.Location = new System.Drawing.Point(1233, 34);
+            this.lab_end.Name = "lab_end";
+            this.lab_end.Size = new System.Drawing.Size(59, 17);
+            this.lab_end.TabIndex = 19;
+            this.lab_end.Text = "lab_end";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(305, 117);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1690, 944);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lab_end);
+            this.Controls.Add(this.lab_time);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.lab_totaltime);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btn_Start);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.btn_OpenFile);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button btn_Start;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btn_OpenFile;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label lab_totaltime;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label lab_time;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lab_end;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ 0805/0805/Form1.cs
@@ -0,0 +1,285 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Data.OleDb;
+using System.Timers;
+
+namespace _0805
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitTimer();
+        }
+        int[] myArry = { 1, 2, 3 };
+        char[] myAStr = { 'a', 'b', 'c' };
+        byte[] myByte = { 001, 002 };
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string myPath = "a.txt";
+            StreamWriter sw = new StreamWriter(myPath);
+            foreach(char myChar in myAStr)
+            {
+                sw.Write(myChar);
+            }
+            sw.WriteLine();
+            sw.WriteLine(sw.ToString());
+            sw.Close();
+            StreamReader sr = new StreamReader(myPath);
+            MessageBox.Show("内容为:\n " + sr.ReadToEnd());
+          
+        }
+
+        private int myMaxValue()
+        {
+            int a, b;
+            a = Convert.ToInt32(textBox1.Text);
+            b = Convert.ToInt32(textBox2.Text);
+            return a > b ? a : b;
+        }
+        private void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            MessageBox.Show("message");
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+            label2.Text = Convert.ToString(myMaxValue());
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+            label2.Text = Convert.ToString(myMaxValue());
+        }
+
+        public DataSet getpath()
+        {
+            //打开文件
+            OpenFileDialog file = new OpenFileDialog();
+            file.Filter = "Excel(*.xlsx)|*.xlsx|Excel(*.xls)|*.xls";
+            file.InitialDirectory = "..\\";
+            file.Multiselect = false;
+
+            if (file.ShowDialog() == DialogResult.Cancel)return null;
+
+            //判断文件后缀
+            string  path = file.FileName;
+
+            string fileSuffix = System.IO.Path.GetExtension(path);
+
+            if (string.IsNullOrEmpty(fileSuffix))return null;
+            //MessageBox.Show(path + "+++" + fileSuffix);
+            return myExcel(path,fileSuffix);
+        }
+        public DataSet myExcel(string path, string fileSuffix)
+        {
+            //判断Excel文件是2003版本还是2007版本
+            if (fileSuffix == ".xls")
+
+                connString = "Provider=Microsoft.Jet.OLEDB.4.0;" + "Data Source=" + path + ";" + ";Extended Properties=\"Excel 8.0;HDR=YES;IMEX=1\"";
+
+            else
+
+                connString = "Provider=Microsoft.ACE.OLEDB.12.0;" + "Data Source=" + path + ";" + ";Extended Properties=\"Excel 12.0;HDR=YES;IMEX=1\"";
+
+            //读取文件
+
+            OleDbConnection conn = new OleDbConnection(connString);
+            conn.Open();
+            DataTable sTable = conn.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+            string sTable_Rows;
+            for (int i = 0; i < sTable.Rows.Count; i++)
+            {
+                sTable_Rows = sTable.Rows[i][2].ToString().TrimStart('\'').Trim('\'', '$');
+                listBox1.Items.Add(sTable_Rows);
+            }
+            String tableName;
+            foreach (string i in listBox1.Items)
+            {
+                tableName = i + '$';
+                string strExcels = "select * from [" + tableName + "]";
+                using (OleDbDataAdapter cmd = new OleDbDataAdapter(strExcels, conn))
+                {
+                    cmd.Fill(ds,i);     //分别存在DataSet里相同worksheet名称的表中
+                }
+            }
+            if (ds == null || ds.Tables.Count <= 0) return null;
+            return ds;
+        }
+        string path;
+        string fileSuffix;
+        DataTable dt;
+        string connString = "";
+        DataSet ds = new DataSet();
+        private void button2_Click(object sender, EventArgs e)
+        {           
+            dataGridView1.DataSource = null; //每次打开清空内容 
+            dt = getpath().Tables[0];
+            dataGridView1.DataSource =dt ;
+            //修改样式
+            //dataGridView1.Rows[0].DefaultCellStyle.BackColor = Color.FromArgb(0, 255, 255);
+            dataGridView1.CurrentCell = null;
+        }
+
+        
+        private void listBox1_SelectedValueChanged_1(object sender, EventArgs e)
+        {
+            string mySelectName = listBox1.SelectedItem.ToString();
+            dt = ds.Tables[mySelectName];
+            dataGridView1.DataSource = dt;
+            dataGridView1.CurrentCell = null;
+        }
+        //读写 DatagridView
+        int T_delay;
+        //Form myform = new Form3();
+        private void myDatagridView()
+        {
+            DataGridView myView = dataGridView1;
+            int cmd, record=0, count=0, totalcount=1;
+            for ( int i=1;i< myView.Rows.Count;i++)
+            {
+                string conStr = Convert.ToString(myView.Rows[i].Cells[3].Value);
+                if (conStr =="TRUE" || conStr == "真")
+                {
+                    myView.Rows[i].Cells[3].Style.BackColor = Color.LightGreen;
+                    cmd = Convert.ToInt32(myView.Rows[i].Cells[4].Value);
+                    T_delay = Convert.ToInt32(myView.Rows[i].Cells[5].Value);
+                    if (T_delay > 0)
+                    {
+                        lab_totaltime.Text = T_delay.ToString();
+                        timer1.Start();
+                    }
+                    if (cmd == 10) // CMD:10 表示循环开始位置标识; 11 表示循环结束位置标识
+                    {
+                        i = i + 1;  //下一行为循环的第一步
+                        record = i;
+                        totalcount = Convert.ToInt32(myView.Rows[i - 1].Cells[11].Value); // 循环标志行第10列里写了循环数
+                    }
+                    else if (cmd == 11)
+                    {
+                        if (count < totalcount)
+                        {
+                            i = record;
+                            count += 1;
+                            //labelCurrentCount.Text = count.ToString();
+                            //labelResidueCount.Text = (totalcount - count).ToString();
+                        }
+                        else
+                        {
+                            count = 0;
+                        }
+                    }
+                    else if (cmd == 999)
+                    {
+                        lab_end.Text = "测试结束!";
+                    }
+                    else
+                    {
+                        //添加处理函数处
+           
+                    }
+
+                }        
+            }
+        }
+
+        private void myDtime(double T_delay)
+        {
+            myTimer.Start();
+        }
+
+        private void btn_Start_Click(object sender, EventArgs e)
+        {
+            myDatagridView();
+        }
+
+        /// <summary>
+        /// 定义Timer类变量, 不使用Timer控件(占用form 主进程)
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        System.Timers.Timer myTimer = new System.Timers.Timer();
+        //定义委托
+        private void InitTimer()
+        {
+            myTimer.Interval = 1000;
+            myTimer.AutoReset = true; //执行一次还是一直执行
+            myTimer.Enabled = false; 
+            // 是否执行System.Timers.Timer.Elapsed 事件
+            //绑定Elapse事件
+            myTimer.Elapsed += new System.Timers.ElapsedEventHandler(myTimer_Elapsed);          
+        }
+        private void myTimer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            T_delay -= 1;
+            SetTB("倒计时: " + T_delay + "s");
+            if (T_delay <= 0)
+            {
+                myTimer.Stop();
+            }
+        }
+        private delegate void SetTBMethodInvok(String value);
+        private void SetTB(string value)
+        {
+            if (this.InvokeRequired)
+            {
+                this.Invoke(new SetTBMethodInvok(SetTB), value);
+            }
+            else
+            {
+                //myTimer 做的事
+                this.lab_time.Text = value;
+            }
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            int t = 2;
+            do
+            {
+
+                T_delay = Convert.ToInt32(dataGridView1.Rows[t].Cells[5].Value);
+                timer1.Start();
+                dataGridView1.Rows[t].Cells[3].Style.BackColor = Color.LightGreen;
+                t++;
+            } while (t <= dataGridView1.Rows.Count);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            T_delay = (T_delay - 1);
+            lab_time.Text="倒计时: " + T_delay + "s";
+            if (T_delay <= 0)
+            {
+                timer1.Stop();
+                T_delay = 0;
+            }
+        }
+    }
+}
